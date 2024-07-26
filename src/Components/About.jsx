ad94a60914f06a46020f9465bebd9d6df51db614
Suryakant Kaushik <skcomhacker@gmail.com>
2024-07-26 11:57:11 +0000

    First commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Medical Physics student studying at Stockholm University. I enjoy solving problem related to online adaptive workflow with a focus on proton thearpy.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my medical physicist experience I continually look for new and better ways to make online adaptive therapy possible.";
 
 const About = () => {
   return (
